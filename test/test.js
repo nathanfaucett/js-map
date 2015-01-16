4876624e239348f53cc925fdb27c641c977a3bf8
nathanfaucett <nathanfaucett@gmail.com>
2015-01-15 19:11:13 -0600

    wrote object test

--- test/test.js
+++ test/test.js
@@ -3,20 +3,41 @@ var assert = require("assert"),
 
 
 describe("map", function() {
-    it("should loop over array or object and returns array of callbacks functions", function() {
-        var array = [{
-                age: 42,
-                name: "Bob"
-            }, {
-                age: 23,
-                name: "Frank"
-            }],
-            results;
+    it(
+        "should create array/object with the results of calling the callback on every element in this array/object",
+        function() {
+            var array = [{
+                    age: 42,
+                    name: "Bob"
+                }, {
+                    age: 23,
+                    name: "Frank"
+                }],
+                object = {
+                    0: {
+                        age: 42,
+                        name: "Bob"
+                    },
+                    1: {
+                        age: 23,
+                        name: "Frank"
+                    }
+                };
 
-        results = map(array, function(value) {
-            return value.name;
-        });
+            assert.deepEqual(
+                map(array, function(value) {
+                    return value.name;
+                }), ["Bob", "Frank"]
+            );
 
-        assert.deepEqual(results, ["Bob", "Frank"]);
-    });
+            assert.deepEqual(
+                map(object, function(value) {
+                    return value.name;
+                }), {
+                    0: "Bob",
+                    1: "Frank"
+                }
+            );
+        }
+    );
 });
